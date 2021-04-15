3ede4d41ed9da62f9205595b69a9a97b1a14544b
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-15 12:17:30 -0400

    algo unsolved 02

--- 03-Algorithms/02-is-unique/Unsolved/is-unique.js
+++ 03-Algorithms/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,22 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+let isUnique = (arr) => {
+    // let storeUniqueNo = "";
+    // let isTheNoUnique = true
+
+    for(let i=0; i<arr.length; i++){
+
+        for(let j = i + 1; j<arr.length; j++){
+            if(arr[i] == arr[j]){
+                return false
+            }
+        }
+    }
+    return true
+    
+
+};
+
+let result = isUnique([2,4,6,8,4]);
+console.log(result);
